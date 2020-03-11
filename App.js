78c551455be6a8366d529efbc7d5988bca98b69f
daheeahn <deg9810@likelion.org>
2020-03-12 05:55:54 +0900

    #10.4 isLoggedIn part One

--- App.js
+++ App.js
@@ -8,12 +8,16 @@ import {ApolloProvider} from 'react-apollo-hooks';
 import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {InMemoryCache} from 'apollo-cache-inmemory';
+import {ThemeProvider} from 'styled-components';
 import options from './apollo';
 import {persistCache} from 'apollo-cache-persist';
+import styles from './styles';
 
 const App = ({navigation}) => {
   const [loaded, setLoaded] = useState(false);
   const [client, setClient] = useState(null);
+  // null 체크안함 / false 체크했는데 유저가 로그인 안한거 / true ~
+  const [isLoggedIn, setIsLoggedIn] = useState(null); // local storage에 접근해서 유저 로그인 여부를 알아내기 위함
 
   const preLoad = async () => {
     // 비동기라 다시 할거라고? TODO:
@@ -28,6 +32,16 @@ const App = ({navigation}) => {
         cache,
         ...options,
       });
+
+      const isLoggedIn = await AsyncStorage.getItem('isLoggedIn');
+
+      if (isLoggedIn === null || isLoggedIn === false) {
+        // TODO: parse 해야되는데
+        setIsLoggedIn(false);
+      } else {
+        setIsLoggedIn(true);
+      }
+
       setLoaded(true);
       setClient(client);
     } catch (error) {
@@ -40,12 +54,10 @@ const App = ({navigation}) => {
     preLoad();
   }, []);
 
-  const [load, setLoad] = useState(false);
-
-  return loaded && client ? (
+  return loaded && client && isLoggedIn !== null ? ( // null이 아니지 false가 아닌건 아니야! 주의하기
     <ApolloProvider client={client}>
-      <TouchableOpacity onPress={() => navigation.navigate('Sub')}>
-        <Image
+      <ThemeProvider theme={styles}>
+        {/* <Image
           source={{uri: 'https://pbs.twimg.com/media/EF9H2emUUAA5LGj.jpg'}}
           style={{
             width: 300,
@@ -54,12 +66,10 @@ const App = ({navigation}) => {
           }}
           onLoad={event => {
             console.log(event);
-            setLoad(true);
           }}
-        />
-        <Text>{!load ? 'loading...' : 'completed'}</Text>
-        <Icon name="ios-home" size={30} color="#900" />
-      </TouchableOpacity>
+        /> */}
+        <Text>{isLoggedIn ? "I'm in" : "I'm logged out"}</Text>
+      </ThemeProvider>
     </ApolloProvider>
   ) : (
     <Text>loading...</Text>
---
+++ styles.js
@@ -0,0 +1,11 @@
+const styles = {
+  black: '#262626',
+  grey: '#F9F9F9',
+  darkGrey: '#999',
+  lightGrey: '#c7c7c7',
+  red: '#ED4956',
+  blue: '#3897f0',
+  darkBlue: '#003569',
+};
+
+export default styles;
