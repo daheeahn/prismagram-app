ed151d665dd2154859b2c271a35f3ffbb56032ac
daheeahn <deg9810@likelion.org>
2020-03-15 02:16:44 +0900

    #15.1/2 Post Component Swiper / Finishing Post Component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,10 @@
-import {Image} from 'react-native';
+import {Image, Platform} from 'react-native';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import PropTypes from 'prop-types';
 import React from 'react';
+import Swiper from 'react-native-swiper';
+import constants from '../utils/constants';
 import styled from 'styled-components';
 
 const Container = styled.View``;
@@ -19,12 +23,38 @@ const Bold = styled.Text`
 const Location = styled.Text`
   font-size: 12px;
 `;
+const IconsContainer = styled.View`
+  flex-direction: row;
+  margin-bottom: 10px;
+`;
+const IconContainer = styled.View`
+  margin-right: 10px;
+`;
+const InfoContainer = styled.View`
+  padding: 10px;
+`;
+const Caption = styled.Text`
+  margin: 3px 0px;
+`;
+const CommentCount = styled.Text`
+  margin-top: 5px;
+  opacity: 0.5;
+  font-size: 12px;
+`;
 
-// https://cphoto.asiae.co.kr/listimglink/1/2018101828319792_1539836640.png
+const loves = [
+  'https://cphoto.asiae.co.kr/listimglink/1/2018101828319792_1539836640.png',
+  'https://lh3.googleusercontent.com/proxy/Fbb5TO5ApHzdE9FMSIInTXqi8ghIOZIjXg52-PV8ymEYPaPff7FOJOUS7RyMsMgI15gBvHPsELEbGTRtnxpNBO3tvvfU5Ehl2kHirxOndZKKOVwBxJv7qEWQlPJ1zqhhtCXwGp_QjQQ',
+];
 
-const Post = post => {
-  console.log('post', post);
-  const {user, location} = post;
+const Post = ({
+  user,
+  location,
+  files = [],
+  likeCount,
+  caption,
+  comments = [],
+}) => {
   return (
     <Container>
       <Header>
@@ -46,6 +76,60 @@ const Post = post => {
           </HeaderUserContainer>
         </Touchable>
       </Header>
+
+      <Swiper
+        // showsButtons={false}
+        // showsPagination={false}
+        // paginationStyle={{color}}
+        loop={false}
+        style={{height: constants.height / 2.5}}>
+        {/* {files.map(file => { */}
+        {loves.map((file, index) => {
+          return (
+            <Image
+              //   key={file.id}
+              key={index}
+              style={{height: constants.height / 2.5, width: constants.width}}
+              resizeMode="contain"
+              source={{uri: file}}
+            />
+          );
+        })}
+      </Swiper>
+      <InfoContainer>
+        <IconsContainer>
+          <Touchable>
+            <IconContainer>
+              <Ionicons
+                size={28}
+                name={
+                  Platform.OS === 'ios' ? 'ios-heart-empty' : 'md-heart-empty'
+                }
+              />
+            </IconContainer>
+          </Touchable>
+          <Touchable>
+            <IconContainer>
+              <Ionicons
+                size={28}
+                name={Platform.OS === 'ios' ? 'ios-text' : 'md-text'}
+              />
+            </IconContainer>
+          </Touchable>
+        </IconsContainer>
+        <Touchable>
+          <Bold>{likeCount === 1 ? '1 like' : `${likeCount} likes`}</Bold>
+        </Touchable>
+        <Caption>
+          <Bold>{user.username}</Bold>
+          {caption}
+        </Caption>
+        <Touchable>
+          <CommentCount>
+            [Challenge] See all {comments.length} comments
+          </CommentCount>
+        </Touchable>
+      </InfoContainer>
     </Container>
   );
 };
